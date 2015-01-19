c6816198c11aab696d6bdd8f7b01c36bcc365ebf
unknown <aca14tph@EA200467.windows.dcs.shef.ac.uk>
2015-01-19 13:48:05 +0000

    Added more code

--- Robot.java
+++ Robot.java
@@ -1,6 +1,18 @@
-class Robot {
+/* A program to allow a robot to navigate an obstacle course
+* Written by: Tom Hughes and Mat Gomes
+* First written: 19/01/2015
+* Last updated: 19/01/2015
+*/
+import icommand.nxt.*;
+import icommand.nxt.comm.NXTCommand;
+public class Robot { 
+	//declare objects to represent sensors here
 	
-	public static void main(String[] args){
-
-	}
-}
+	public static void main (String[] args) {
+	
+		NXTCommand.open();
+		NXTCommand.setVerify(true);
+		//statements to control the robot
+		
+		NXTCommmand.close
+}}
